bc0ee799907eacecea472b5a24a56fbd5d1b5058
Erik Lavander <erik.lavander@gmail.com>
2014-12-20 21:07:19 +0000

    More related_names...

--- web/models.py
+++ web/models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.files import File
@@ -9,10 +10,22 @@ import os
 import StringIO
 
 # Create your models here.
+class Venue(models.Model):
+    name = models.CharField(max_length=128, verbose_name='Namn')
+    audience_min = models.IntegerField(blank=True)
+    audience_max = models.IntegerField(blank=True)
+
+    def get_absolute_url(self):
+        return reverse('venue_detail', kwargs={'pk': self.pk})
+
+    def __unicode__(self):
+        return self.name
+
 class Description(models.Model):
-    author = models.ForeignKey(User)
-    text = models.TextField(max_length=512)
+    author = models.ForeignKey(User, verbose_name="Användare")
+    text = models.TextField(max_length=512, verbose_name="Beskrivning")
     timestamp = models.DateTimeField(auto_now_add=True)
+    venue = models.ForeignKey(Venue, related_name="descriptions")
 
     class Meta:
         ordering = ['-timestamp']
@@ -21,31 +34,32 @@ class Description(models.Model):
         # Return the first five words.
         return ' '.join(self.text.split()[:5])
 
-class Contact(models.Model):
-    name = models.CharField(max_length=128, blank=True)
-    email = models.EmailField(blank=True)
-    phone = models.CharField(max_length=128, blank=True, null=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    added_by = models.ForeignKey(User)
+class Address(models.Model):
+    author = models.ForeignKey(User, verbose_name="Användare")
+    text = models.TextField(max_length=512, verbose_name="Adress")
+    venue = models.OneToOneField(Venue, blank=True, related_name="address")
 
     def __unicode__(self):
-        return self.name
+        return '{venue}, {address}'.format(venue=self.venue.name, address=self.text)
 
-class Equipment(models.Model):
-    name = models.CharField(max_length=128)
-    desc = models.TextField(max_length=512, blank=True)
+class Contact(models.Model):
+    name = models.CharField(max_length=128, blank=True, verbose_name="Namn")
+    email = models.EmailField(blank=True)
+    phone = models.CharField(max_length=128, blank=True, null=True, verbose_name="Telefon")
     timestamp = models.DateTimeField(auto_now_add=True)
     added_by = models.ForeignKey(User)
+    venue = models.ForeignKey(Venue, related_name="contacts")
 
     def __unicode__(self):
         return self.name
 
 class Image(models.Model):
-    caption = models.CharField(max_length=128, blank=True)
-    image = models.ImageField(upload_to='images')
+    caption = models.CharField(max_length=128, null=True, blank=True)
+    image = models.ImageField(upload_to='images', null=True, blank=True)
     thumbnail = models.ImageField(upload_to='thumbnails', blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     added_by = models.ForeignKey(User)
+    venue = models.ForeignKey(Venue, related_name="images")
 
     def make_thumbnail(self):
         image = PILImage.open(self.image.path)
@@ -66,21 +80,3 @@ class Image(models.Model):
         else:
             return "Image {pk}".format(self.pk)
 
-class Venue(models.Model):
-    name = models.CharField(max_length=128)
-    address = models.TextField(max_length=512, blank=True)
-    descriptions = models.ManyToManyField(Description, blank=True)
-    contacts = models.ManyToManyField(Contact, blank=True)
-    images = models.ManyToManyField(Image, blank=True)
-    audience_min = models.IntegerField(blank=True)
-    audience_max = models.IntegerField(blank=True)
-    equipment = models.ManyToManyField(Equipment, blank=True)
-
-    # def latest_update(self):
-    #     latest = self.descriptions.order_by('-timestamp')
-
-    def get_absolute_url(self):
-        return reverse('venue_detail', kwargs={'pk': self.pk})
-
-    def __unicode__(self):
-        return self.name

620a59f6d6fb5c37fa9bca7d189075d272f19d4f
Erik Lavander <erik.lavander@gmail.com>
2014-12-21 23:24:41 +0000

    ...and finally, we've got protected views. (Everything except the landing page is protected at the moment, there should perhaps be read-only versions of search, venue_list and venue_detail.)

--- web/urls.py
+++ web/urls.py
@@ -1,20 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView, DetailView, ListView, FormView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import logout
+from django.contrib.auth.decorators import login_required
 from web.models import Venue
 from web.views import LandingPage, AdvancedSearchView, VenueList, VenueDetail, CreateVenue, DeleteVenue, CreateDescription, CreateContact, CreateImage
 
 urlpatterns = patterns('',
-    url(r'^$', VenueList.as_view(), name='venue_list'),
-    url(r'^venue/(?P<pk>\d+)/$', VenueDetail.as_view(), name='venue_detail'),
-    url(r'^venue/(?P<pk>\d+)/delete$', DeleteVenue.as_view(), name='delete_venue'),
-    url(r'^venue/(?P<pk>\d+)/create_description$', CreateDescription.as_view(), name='create_description'),
-    url(r'^venue/(?P<pk>\d+)/create_contact$', CreateContact.as_view(), name='create_contact'),
-    url(r'^venue/(?P<pk>\d+)/create_image$', CreateImage.as_view(), name='create_image'),
-    url(r'^create_venue$', CreateVenue.as_view(), name='create_venue'),
-    url(r'^search$', AdvancedSearchView.as_view(), name='search'),
+    url(r'^$', login_required(VenueList.as_view()), name='venue_list'),
+    url(r'^venue/(?P<pk>\d+)/$', login_required(VenueDetail.as_view()), name='venue_detail'),
+    url(r'^venue/(?P<pk>\d+)/delete$', login_required(DeleteVenue.as_view()), name='delete_venue'),
+    url(r'^venue/(?P<pk>\d+)/create_description$', login_required(CreateDescription.as_view()), name='create_description'),
+    url(r'^venue/(?P<pk>\d+)/create_contact$', login_required(CreateContact.as_view()), name='create_contact'),
+    url(r'^venue/(?P<pk>\d+)/create_image$', login_required(CreateImage.as_view()), name='create_image'),
+    url(r'^create_venue$', login_required(CreateVenue.as_view()), name='create_venue'),
+    url(r'^search$', login_required(AdvancedSearchView.as_view()), name='search'),
 
     url(r'^accounts/login', LandingPage.as_view(), name='landingpage'),
-    url(r'accounts/profile', TemplateView.as_view(template_name="profile.html"), name='profile'),
+    url(r'accounts/profile', login_required(TemplateView.as_view(template_name="profile.html")), name='profile'),
     url(r'accounts/logout', logout, {'next_page': '/'}, name='logout'),
 )
